bd928d181e19c6319a9149988d0c80570f34fe87
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-04-01 23:36:33 -0300

    Importação do cors e instanciação para uso na aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()  //instanciação da aplicação
 
+app.use(cors())
 app.use(express.json())  //requisições no formato json
 app.use(routes)
 
