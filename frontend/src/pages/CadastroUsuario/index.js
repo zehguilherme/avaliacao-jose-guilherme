625d1416acd758ab10191b35c2aa0604a42b8a1b
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-04-01 23:26:59 -0300

    Finalização da página de cadastro de usuários (index.js e css)

--- frontend/src/pages/CadastroUsuario/index.js
+++ frontend/src/pages/CadastroUsuario/index.js
@@ -1,35 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom' /*Adição de link na página*/
+
+import api from '../../services/api'
 
 import './styles.css'
 
 export default function CadastroUsuario () {
+
+  // Criação de estados para cada um dos inputs
+  const [user_name, setUser_name] = useState('')  //Nome de usuário: input de texto -> inicia com valor vazio ''
+  const [email, setEmail] = useState('')          //E-mail
+  const [phone, setPhone] = useState('')          //Telefone
+  const [password, setPassword] = useState('')    //Senha
+
+  const history = useHistory()
+
+  // Responsável pelo cadastro do usuário - disparada quando o form der 'submit'
+  async function cadastroUsuario (e) {
+    e.preventDefault()  //vai previnir o comportamento padrão do form (recarregar página toda)
+
+    const data = {
+      user_name,
+      email,
+      phone,
+      password
+    }
+
+    try {
+      await api.post('users', data)  //response
+
+      history.push('/')  //envia o usuário para a página de Login
+
+    } catch (error) {
+      alert('Erro no cadastro, tente novamente')
+    }
+  }
+
+  /*******************************************************************************************************************************/
+
   return (
-    <div className="cadastro-container">
+    <div className="cadastro-usuario-container">
       <section className="form">
-        <form action="">
+        <form onSubmit={cadastroUsuario}>
           <h1>Cadastro de Usuário</h1>
 
-          <div id="user_name">
-            <label htmlFor="">Nome de usuário</label>
-            <input type="text" name="user-name-input" id="user-name-input" required={true} />
+          <div id="user-name">
+            <input
+              type="text"
+              name="user-name-input"
+              id="user-name-input"
+              placeholder="Nome de usuário"
+              required={true}
+              value={user_name}
+              onChange={e => setUser_name(e.target.value)}
+            />
           </div>
 
-          <div id="cpf">
-            <label htmlFor="">E-mail</label>
-            <input type="text" name="cpf-input" id="cpf-input" required={true} />
+          <div id="email">
+            <input
+              type="email"
+              name="email-input"
+              id="email-input"
+              placeholder="E-mail"
+              required={true}
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
           </div>
 
           <div id="phone">
-            <label htmlFor="">Telefone</label>
-            <input type="text" name="phone-input" id="phone-input" required={true} />
+            <input
+              type="text"
+              name="phone-input"
+              id="phone-input"
+              placeholder="Telefone"
+              required={true}
+              value={phone}
+              onChange={e => setPhone(e.target.value)}
+            />
           </div>
 
           <div id="password">
-            <label htmlFor="">Senha</label>
-            <input type="password" name="password-input" id="password-input" required={true} />
+            <input
+              type="password"
+              name="password-input"
+              id="password-input"
+              placeholder="Senha"
+              required={true}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
           </div>
 
           <button type="submit">Cadastrar</button>
+
+          <Link to={'/'}>
+            Voltar para a home
+          </Link>
         </form>
       </section>
     </div>
