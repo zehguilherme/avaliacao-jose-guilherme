e4e753f65e915d084569367c564d4834bdf469c2
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-04-01 23:34:55 -0300

    Remoção de comentários e adição da rota de Login (post)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,15 +2,19 @@ const express = require('express')
 
 const UserController = require('./controllers/UserController')
 const ClientController = require('./controllers/ClientController')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()  //modulo de rotas do express passa para a variavel em questão
 
 // Usuário
 routes.post('/users', UserController.create) // Criar / cadastrar
-// routes.get('/users', UserController.index)   // Listar / consultar
+routes.get('/users', UserController.index)   // Listar / consultar
 
 // Cliente
 routes.post('/clients', ClientController.create)  // Criar / cadastrar
-// routes.get('/clients', ClientController.index)   // Listar / consultar
+routes.get('/clients', ClientController.index)   // Listar / consultar
+
+// Login - criar uma sessão (post)
+routes.post('/sessions', SessionController.create)
 
 module.exports = routes
